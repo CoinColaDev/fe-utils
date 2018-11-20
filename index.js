56a6f98e6d728b2769f1d61fc8776fc24cde49d8
simongfxu <xugaofan@gmail.com>
2018-11-20 13:47:18 +0800

    Add entry file

--- index.js
+++
@@ -1,7 +0,0 @@
-export const im = require('./lib/RongMessage')
-
-export const logger = require('./lib/logger')
-
-export const ajax = require('./lib/ajax')
-
-export const cookie = require('./lib/cookie')
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export const im = require('./RongMessage')
+
+export const logger = require('./logger')
+
+export const ajax = require('./ajax')
+
+export const cookie = require('./cookie')
