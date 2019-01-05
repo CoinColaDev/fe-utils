4f56fa8f1498c90e3a3d6488beb7b70764e12452
simongfxu <xugaofan@gmail.com>
2019-01-05 16:15:13 +0800

    adapter 只在调试时使用

--- src/mobile/ccbridge.adapter.js
+++ src/mobile/ccbridge.adapter.js
@@ -4,29 +4,31 @@
 
 import ccbridge, { modules } from "./ccbridge"
 
-const noop = (modName, methodName) => {
-  console.log(
-    `**** Adpater for ccbrige.jsbridge.${modName}.${methodName} ****`
-  )
-  return function() {
-    console.log(`**** ccbridge adapter method ****`)
-    console.log(modName, methodName, arguments)
+if (process.env.NODE_ENV === 'development') {
+  const noop = (modName, methodName) => {
+    console.log(
+      `**** Adpater for ccbrige.jsbridge.${modName}.${methodName} ****`
+    )
+    return function() {
+      console.log(`**** ccbridge adapter method ****`)
+      console.log(modName, methodName, arguments)
+    }
   }
-}
-
-ccbridge.onReady(injected => {
-  if (injected === false) {
-    const { jsbridge } = ccbridge
-    modules.forEach(mod => {
-      mod.methods.forEach(method => {
-        if (!jsbridge[mod.name]) {
-          jsbridge[mod.name] = {}
-        }
-        jsbridge[mod.name][method] = noop(mod.name, method)
+  
+  ccbridge.onReady(injected => {
+    if (injected === false) {
+      const { jsbridge } = ccbridge
+      modules.forEach(mod => {
+        mod.methods.forEach(method => {
+          if (!jsbridge[mod.name]) {
+            jsbridge[mod.name] = {}
+          }
+          jsbridge[mod.name][method] = noop(mod.name, method)
+        })
       })
-    })
-
-    jsbridge.ui.setPageTitle = title => (document.title = title)
-    jsbridge.ui.openWebPage = url => (window.location.href = url)
-  }
-})
+  
+      jsbridge.ui.setPageTitle = title => (document.title = title)
+      jsbridge.ui.openWebPage = url => (window.location.href = url)
+    }
+  })
+}
